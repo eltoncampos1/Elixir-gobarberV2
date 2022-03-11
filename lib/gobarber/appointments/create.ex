45ee60a47e61857029e86babffa27accc288f2d3
eltoncampos1 <eltoncampos36@gmail.com>
2022-03-11 17:58:18 -0300

    feat: create varification to dont create two appointments in the same date

--- lib/gobarber/appointments/create.ex
+++ lib/gobarber/appointments/create.ex
@@ -1,13 +1,27 @@
-defmodule Gobarber.Appointments.Create do
-  alias Gobarber.{Appointment, Repo}
+    defmodule Gobarber.Appointments.Create do
+      alias Gobarber.{Appointment, Repo}
+      alias Gobarber.Appointments.Find
 
-  def call(params) do
-    params
-    |> Appointment.changeset()
-    |> Repo.insert()
-    |> handle_insert()
-  end
+      def call(params) do
+        case validate_date_availability(params) do
+          {:error, reason} ->
+            {:error, reason}
 
-  defp handle_insert({:ok, %Appointment{} = result}), do: {:ok, result}
-  defp handle_insert({:error, result}), do: {:error, result}
-end
+          {:ok, _} ->
+            params
+            |> Appointment.changeset()
+            |> Repo.insert()
+            |> handle_insert()
+        end
+      end
+
+      defp handle_insert({:ok, %Appointment{} = result}), do: {:ok, result}
+      defp handle_insert({:error, result}), do: {:error, result}
+
+      defp validate_date_availability(%{provider: _provider, date: date} = _params) do
+        case Find.by_date(date) do
+          %Appointment{} -> {:error, "Appointment already booked"}
+          nil -> {:ok, nil}
+        end
+      end
+    end
