64ae31ee90fbf807f34411f35b7ea5bb2e3d7294
eltoncampos1 <eltoncampos36@gmail.com>
2022-03-16 13:58:19 -0300

    feat: create error view to handle with fallback controller

--- lib/gobarber_web/views/error_view.ex
+++ lib/gobarber_web/views/error_view.ex
@@ -1,5 +1,6 @@
 defmodule GobarberWeb.ErrorView do
   use GobarberWeb, :view
+  import Ecto.Changeset, only: [traverse_errors: 2]
 
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
@@ -10,7 +11,31 @@ defmodule GobarberWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
+
   def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+
+  def render("error.json", %{result: %Ecto.Changeset{} = changeset}) do
+    %{
+      message: translate_errors(changeset)
+    }
+  end
+
+  def render("error.json", %{result: result}) do
+    %{
+      message: result
+    }
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", translate_value(value))
+      end)
+    end)
+  end
+
+  defp translate_value({:parameterized, Ecto.Enum, _map}), do: ""
+  defp translate_value(value), do: to_string(value)
 end
